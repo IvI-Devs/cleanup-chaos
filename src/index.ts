3f996907f59e81f2d1c5780a0714f0c5dd633eef
gkkconan <conan.gkk@gmail.com>
2025-03-27 09:24:13 +0100

    feat: delete minimap code from intro and create a dedicated scene

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import Intro from "./scenes/Intro";
 import Options from "./scenes/screens/options";
 import Credits from "./scenes/screens/credits";
 import Exit from "./scenes/easter\ eggs/exit";
+import Minimap from "./scenes/minimap";
 import { GameData } from "./GameData"; // global game data
 
 window.addEventListener("load", () => {
@@ -31,7 +32,8 @@ window.addEventListener("load", () => {
       GameOver,
       Options,
       Credits,
-      Exit
+      Exit,
+      Minimap
     ],
     physics: {
       default: "arcade",
